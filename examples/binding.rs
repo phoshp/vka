de314ac382b34f0acd883b7c86c92013591d09ac
phoshp <emre.yavuz.842@gmail.com>
2026-05-21 21:09:03 +0300

    fix example: binding

--- examples/binding.rs
+++ examples/binding.rs
@@ -4,15 +4,15 @@ use ash::vk;
 use vka;
 use vka::RenderingDeviceDesc;
 
-pub fn main() -> vka::Result<()> {
+pub fn main() {
     env_logger::init();
-    let rd = vka::RenderingDevice::new(&RenderingDeviceDesc::default().with_gpu_validation())?;
+    let rd = vka::RenderingDevice::new(&RenderingDeviceDesc::default()).unwrap();
 
-    let buffer = rd.buffer_create(&vka::BufferDesc::uniform(4 * 1024))?;
-    let image = rd.image_create(&vka::ImageDesc::new_2d(vk::Format::R8G8B8A8_UNORM, 256, 256))?;
-    let sampler = rd.sampler_nearest(vk::SamplerAddressMode::REPEAT);
+    let buffer = rd.new_buffer(&vka::BufferDesc::uniform(4 * 1024));
+    let image = rd.new_image(&vka::ImageDesc::new_2d(vk::Format::R8G8B8A8_UNORM, 256, 256));
+    let sampler = rd.new_sampler_nearest(vk::SamplerAddressMode::REPEAT);
 
-    let set_layout = rd.descriptor_set_layout_create(&[
+    let set_layout = rd.new_descriptor_set_layout(&[
         vka::DescriptorSetLayoutEntry {
             binding: 0,
             ty: vk::DescriptorType::UNIFORM_BUFFER,
@@ -31,13 +31,12 @@ pub fn main() -> vka::Result<()> {
             count: 1,
             flags: None,
         },
-    ])?;
-    let set = rd.descriptor_set_create(&set_layout)?;
-    rd.descriptor_set_write(&set, &[
+    ]);
+    let set = rd.new_descriptor_set(&set_layout);
+    rd.write_descriptors(&set, &[
         vka::WriteDescriptor::Buffer { binding: 0, array_element: 0, infos: &[buffer.descriptor(0, vk::WHOLE_SIZE)] },
-        vka::WriteDescriptor::Image { binding: 1, array_element: 0, infos: &[rd.image_full_view(&image).descriptor()] },
-        vka::WriteDescriptor::Image { binding: 2, array_element: 0, infos: &[vk::DescriptorImageInfo::default().sampler(sampler.value)] },
+        vka::WriteDescriptor::Image { binding: 1, array_element: 0, infos: &[image.full_view().descriptor()] },
+        vka::WriteDescriptor::Image { binding: 2, array_element: 0, infos: &[vk::DescriptorImageInfo::default().sampler(sampler.raw)] },
     ]);
     // TODO: test descriptors in a pipeline.
-    Ok(())
 }
