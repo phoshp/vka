049cfeead98c218d639dcac2a4f99ea462b6a51c
phoshp <emre.yavuz.842@gmail.com>
2026-08-05 10:53:08 +0300

    ok

--- src/image.rs
+++ src/image.rs
@@ -150,13 +150,18 @@ fn conv_format_to_aspect_mask(format: vk::Format) -> vk::ImageAspectFlags {
 }
 
 pub fn find_optimal_image_layout(usage: vk::ImageUsageFlags) -> vk::ImageLayout {
+    let sampled = usage.contains(vk::ImageUsageFlags::SAMPLED) || usage.contains(vk::ImageUsageFlags::INPUT_ATTACHMENT);
     if usage.contains(vk::ImageUsageFlags::STORAGE) {
         vk::ImageLayout::GENERAL
     } else if usage.contains(vk::ImageUsageFlags::COLOR_ATTACHMENT) {
-        vk::ImageLayout::COLOR_ATTACHMENT_OPTIMAL
+        if sampled {
+            vk::ImageLayout::GENERAL
+        } else {
+            vk::ImageLayout::COLOR_ATTACHMENT_OPTIMAL
+        }
     } else if usage.contains(vk::ImageUsageFlags::DEPTH_STENCIL_ATTACHMENT) {
-        if usage.contains(vk::ImageUsageFlags::SAMPLED) || usage.contains(vk::ImageUsageFlags::INPUT_ATTACHMENT) {
-            vk::ImageLayout::DEPTH_STENCIL_READ_ONLY_OPTIMAL
+        if sampled {
+            vk::ImageLayout::GENERAL
         } else {
             vk::ImageLayout::DEPTH_STENCIL_ATTACHMENT_OPTIMAL
         }
